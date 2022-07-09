921c7664ed271526da25ba16b67eef35963bf822
Eminem-x <1773997586@qq.com>
2022-07-09 13:12:52 +0800

    Create:html css js urls

--- acapp/scripts/compress_game_js.sh
+++ acapp/scripts/compress_game_js.sh
@@ -4,4 +4,4 @@ JS_PATH=/home/ycx/Web/acapp/game/static/js/
 JS_PATH_DIST=${JS_PATH}dist/
 JS_PATH_SRC=${JS_PATH}src/
 
-find $JS_PAth_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
+find $JS_PATH_SRC -type f -name '*.js' | sort | xargs cat > ${JS_PATH_DIST}game.js
